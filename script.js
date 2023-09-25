af74a1a42be4863c26912b362fe1fff0679e3682
Manimozhi A <145418499+mozhinag@users.noreply.github.com>
2023-09-25 22:11:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var request = new XMLHttpRequest();
+
+request.open("GET","https://restcountries.com/v3.1/all");
+
+request.send();
+
+
+request.onload=function(){
+    var result = JSON.parse(request.response);
+    console.log(result);
+    var flag =""
+    for(var x in result){
+        console.log(result[x].flag);
+    }
+
+
+
+
+}
